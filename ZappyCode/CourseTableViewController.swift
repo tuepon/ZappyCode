0f9aae9ccd75af38ee1b404e0c744bcecf144dd0
Tomonori Ueno <t.uepon@gmail.com>
2021-06-27 22:26:48 +0700

    Fixed defect in CourseTableViewController

--- ZappyCode/CourseTableViewController.swift
+++ ZappyCode/CourseTableViewController.swift
@@ -31,14 +31,16 @@ class CourseTableViewController: UITableViewController {
         }
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
 
+    
 }
