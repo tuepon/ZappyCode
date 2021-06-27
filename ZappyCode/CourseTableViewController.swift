8146ed59c2881b73912bc48f6ea48308d584f652
Tomonori Ueno <t.uepon@gmail.com>
2021-06-27 15:17:42 +0700

    Fixing func getCourses on CourseTableViewController

--- ZappyCode/CourseTableViewController.swift
+++ ZappyCode/CourseTableViewController.swift
@@ -2,7 +2,7 @@
 //  CourseTableViewController.swift
 //  ZappyCode
 //
-//  Created by SEZAX development on 14/05/2021.
+//  Created by SEZAX development on 27/06/2021.
 //
 
 import UIKit
@@ -19,10 +19,18 @@ class CourseTableViewController: UITableViewController {
         if let url = URL(string: "https://zappycode.com/api/courses") {
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
-            URLSession.shared.dataTask(with: request)
+            URLSession.shared.dataTask(with: request) {(data, response, error) in
+                
+                if error != nil {
+                    print("There was an error")
+                } else if data != nil {
+                    print(String(data: data!, encoding: .utf8))
+                }
+                
+            }.resume()
         }
     }
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -31,14 +39,6 @@ class CourseTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
     }
 
 }
